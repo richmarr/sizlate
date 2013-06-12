dc9f1d8e06a89302f888be2174afe984b887cfd2
Richard Marr <richard.marr@gmail.com>
2013-06-12 16:52:35 +0100

    Split out separate use cases to try and catch all the code paths

--- spec/file.spec.js
+++ spec/file.spec.js
@@ -1,6 +1,85 @@
 var sizlate = require('../sizlate.js'),
 	cheerio = require("cheerio");
 
+describe('When given a file', function() {
+	it("it should render properly", function(done) {
+		sizlate.__express('spec/fixtures/view-a.html', {
+			settings : {
+				views : "spec/fixtures",
+				'view engine':"html"
+			}
+		}, function( err, html ){
+			
+			var $domNode,
+				$ = cheerio.load(html);
+		
+			$domNode = $("#partials");
+			expect($domNode.length).toEqual(1);
+			
+			done();
+		});
+	});
+});
+
+
+describe('When given a file and a layout', function() {
+	it("it should render properly", function(done) {
+		sizlate.__express('spec/fixtures/view-a.html', {
+			settings : {
+				views : "spec/fixtures",
+				'view engine':"html"
+			},
+			layout:"layout-a"
+		}, function( err, html ){
+			
+			var $domNode,
+				$ = cheerio.load(html);
+		
+			$domNode = $("#partials");
+			expect($domNode.length).toEqual(1);
+			
+			$domNode = $("html");
+			expect($domNode.length).toEqual(1);
+			
+			done();
+		});
+	});
+});
+
+
+describe('When given a file with partials', function() {
+	it("it should pull them in properly", function(done) {
+		sizlate.__express('spec/fixtures/view-a.html', {
+			settings : {
+				views : "spec/fixtures",
+				'view engine':"html"
+			},
+			selectors : {
+				'#partials': {
+					partial: 'partial-a',
+					data: [
+						{ name: 'bob' },
+						{ name: 'anna' }
+					]
+				}
+			}
+		}, function( err, html ){
+			
+			var $domNode,
+				$ = cheerio.load(html);
+			
+			$domNode = $(".partial.a");
+			expect($domNode.length).toEqual(2);
+			
+			$domNode = $("#partials");
+			expect($domNode.length).toEqual(1);
+			
+			done();
+		});
+	});
+});
+
+
 describe('When given a file with layout and partials', function() {
 	it("it should pull them together properly", function(done) {
 		sizlate.__express('spec/fixtures/view-a.html', {
